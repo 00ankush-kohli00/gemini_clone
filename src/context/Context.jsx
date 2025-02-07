f5e1529ea90559c36b4513c3d3df537b843666dc
 <ankushkohli873@gmail.com>
2025-02-07 20:52:20 +0530

    gemini api intigration

---
+++ src/context/Context.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import "../config/gemini";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultDate, setResultDate] = useState("");
+
+  const onSent = async (prompt) => {
+    await run(prompt);
+  };
+  //   onSent("what is react js");
+  const contextValue = {};
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
